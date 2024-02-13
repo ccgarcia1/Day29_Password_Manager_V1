f90a811e7613167646479b1a1cce08bfd3a63ef9
ccgarcia1 <42050096+ccgarcia1@users.noreply.github.com>
2024-02-12 23:22:41 -0300

    Generate password function

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 from tkinter import *
 from tkinter import messagebox
+from random import choice, randint, shuffle
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    # using list comprehension to get the random letters
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    # using list comprehension to get the random symbols
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    # using list comprehension to get the random numbers
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
-
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
@@ -13,13 +34,13 @@ def save_password():
         messagebox.showinfo(title="Ooops", message="The Website field or Password field cannot be blank")
 
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"Email: {email}\nPassword: {password}\n\nSave this info?")
+        is_ok = messagebox.askokcancel(title=website,
+                                       message=f"Email: {email}\nPassword: {password}\n\nSave this info?")
         if is_ok:
             with open(r"pass.txt", mode="a") as file:
                 file.write(f"{website} || {email} || {password}\n")
                 website_entry.delete(0, END)
                 password_entry.delete(0, END)
-
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -51,15 +72,9 @@ password_entry = Entry(width=33)
 password_entry.grid(row=3, column=1)
 
 # buttons
-generate_password_button = Button(text="Generate Password")
+generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2, columnspan=1)
 add_button = Button(text="Add", width=44, command=save_password)
 add_button.grid(row=4, column=1, columnspan=2)
-
-
-
-
-
-
 
 window.mainloop()
