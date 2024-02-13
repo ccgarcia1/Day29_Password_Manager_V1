b03ed82731dd0409b89d57bde09f0441fb816b05
ccgarcia1 <42050096+ccgarcia1@users.noreply.github.com>
2024-02-12 22:48:39 -0300

    Validation of blank fields and message box

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -7,8 +8,18 @@ def save_password():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
-    with open(r"pass.txt", mode="a") as file:
-        file.write(f"{website} || {email} || {password}\n")
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Ooops", message="The Website field or Password field cannot be blank")
+
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"Email: {email}\nPassword: {password}\n\nSave this info?")
+        if is_ok:
+            with open(r"pass.txt", mode="a") as file:
+                file.write(f"{website} || {email} || {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
+
 
 
 # ---------------------------- UI SETUP ------------------------------- #
