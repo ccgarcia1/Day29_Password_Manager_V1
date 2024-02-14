97dcf6a78b4cacf3cab7009140197de7323ee96a
ccgarcia1 <42050096+ccgarcia1@users.noreply.github.com>
2024-02-13 21:04:30 -0300

    Adding the pyperclip to copy the code to clipboard

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
-
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 def generate_password():
@@ -23,6 +23,7 @@ def generate_password():
     password = "".join(password_list)
     password_entry.insert(0, password)
     pyperclip.copy(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
